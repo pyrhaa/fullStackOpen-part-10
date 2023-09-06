2e9362d82d008ee1f43c643d9cbc989e32942dd9
pyrhaa <sboubtane@protonmail.com>
2023-09-06 15:55:21 +0200

    10.2 ok

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Azul fellawen Tamazgha</Text>
+      <Text>Azul fellawen Tamazgha !</Text>
       <StatusBar style="auto" />
     </View>
   );
