0cfb86319160dcef634cba07fc2e46b40ec25f75
Sarah <sboubtane@protonmail.com>
2024-08-10 10:08:07 +0200

    10.8 in progress, to do obscure password with secureTextEntry

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,76 @@
-import Text from './Text';
+import { Text, TextInput, Pressable, View, StyleSheet } from 'react-native';
+import { useFormik } from 'formik';
+import theme from '../theme'
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    display: "flex",
+    flexDirection: "column",
+  },
+  textInput: {
+    margin: 10,
+    paddingHorizontal: 9,
+    paddingVertical: 3,
+    borderWidth: 1,
+    borderRadius: 9,
+    borderColor: "#b6b6b6",
+  },
+  signButton: {
+    backgroundColor: theme.colors.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 9,
+  },
+  textColor: {
+    color: "white",
+  },
+})
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const SignInForm = ({onSubmit}) => {
+
+  const formik = useFormik({
+      initialValues,
+      onSubmit,
+    });
+
+  return (
+    <View style={styles.container}>
+    <Text>The sign-in view</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Password"
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+      />
+      <Pressable style={styles.signButton} onPress={formik.handleSubmit}>
+        <Text style={styles.textColor}>Sign in</Text>
+      </Pressable>
+    </View>
+  )
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = values => {
+    const username = values.username;
+    const password = values.password;
+
+    console.log(`Your username: ${username} and your password: ${password}`);
+    
+  };
+
+  return <SignInForm onSubmit={onSubmit} />;
 };
 
 export default SignIn;
